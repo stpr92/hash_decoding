5196278d086ae9542fc56717efc2ed4d02ac56ca
stpr92 <siavash.tehranipoor92@gmail.com>
2024-07-12 15:43:28 +0200

    hash256.py

--- hash256.py
+++ hash256.py
@@ -7,7 +7,7 @@ def hash_password_hack(input_file_name, output_file_name):
         with open(f'{output_file_name}.csv','w',newline='') as final :
             writer=csv.writer(final)
             a=dict()
-            for i in range(1000,10000) :
+            for i in range(1000,10000) :     #It is guaranteed that the passwords are 4 digits long.
                 hash=hashlib.sha256(str(i).encode('utf-8')).hexdigest()
                 a[hash]=str(i)
             for row in f :
